eac2fbfa713e4a329a31fc0ef33a76fd44036426
Lourenco Pacheco <lou@Lourencos-MacBook-Pro.local>
2023-11-30 15:00:09 +0000

    changes

--- part1/client.cpp
+++ part1/client.cpp
@@ -7,16 +7,19 @@
 #include <string.h>
 #include <iostream>
 #include <signal.h>
+#include <errno.h>
+#include <string>
+#include <vector>
 
 #define BUFSIZE 128
 
+using namespace std;
+
 extern int errno;
 
-int password = 0;
-char UID[6] = "\0";
-char AID[6] = "\0";
-
-using namespace std;
+string password = "\0";
+string UID = "\0";
+string AID = "\0";
 
 /*
 * Checks if condition is true. In that case, exits with error code 1.
@@ -29,7 +32,7 @@ void safe_stop(int signal) {
     exit(0);
 }
 
-char* udp_string_analysis(char *str) {
+string string_analysis(char *str) {
 
     char *token, *token2, *token3;
 
@@ -38,14 +41,14 @@ char* udp_string_analysis(char *str) {
     token3 = strtok(NULL, " ");
 
     if (token == NULL) {
-        return -1;
+        return "";
     } else if(token2 == NULL) {
         if(!strcmp(token, "logout") && token2 == NULL) {
             return "LOU " + UID + " " + password;
         } else if(!strcmp(token, "unregister")) {
             return "UNR " + UID + " " + password;
         } else if(!strcmp(token, "exit")) {
-            return 3;
+            return "";
         } else if(!strcmp(token, "myauctions") || !strcmp(token, "ma")) {
             return "LMA " + UID;
         } else if(!strcmp(token, "mybids") || !strcmp(token, "mb")) {
@@ -53,30 +56,30 @@ char* udp_string_analysis(char *str) {
         } else if(!strcmp(token, "list") || !strcmp(token, "l")) {
             return "LST";
         }
-        return -1;
+        return "";
     } else if (token3 == NULL && !strcmp(token2, "AID")) {
         strcpy(AID, token2);
         if(!strcmp(token, "show_asset") || !strcmp(token, "sa")) {
-            return 9;
+            return "";
         } else if(!strcmp(token, "close")) {
-            return 5;
+            return "";
         } else if(!strcmp(token, "show_record") || !strcmp(token, "sr")) {
             return "SRC " + AID;
         }
-        return -1;
+        return "";
     } else if (token3 != NULL) {
         if(!strcmp(token, "login") && !strcmp(token2, "UID") && !strcmp(token3, "password")) {
             strcpy(UID, token2); strcpy(password, token3);
             return "LIN " + UID + " " + password;
         } else if(!strcmp(token, "open") && !strcmp(token2, "name") && !strcmp(token3, "asset_fname") 
                     && strcmp(strtok(NULL, " "), "start_value") && strcmp(strtok(NULL, " "), "timeactive")) {
-            return 4;
+            return "";
         } else if((!strcmp(token, "bid") || !strcmp(token, "b")) && !strcmp(token2, "AID") && !strcmp(token3, "value")) {
-            return 10;
+            return "";
         }
-        return -1;
+        return "";
     }
-    return -1;
+    return "";
 }
 
 int tcp_client(char *asip, char *port) {
@@ -96,27 +99,23 @@ int tcp_client(char *asip, char *port) {
     check((connect(fd, res->ai_addr, res->ai_addrlen)) == -1); //connect to server
 
     //TODO : CHECK IF SERVER IS UP
-
-    while(true) {
         
-        check((nread = read(0, buffer, BUFSIZE)) == -1); // read from stdin
-        ptr = buffer; // pointer to buffer
-        ptr[nread-1] = '\0'; // add null terminator to buffer
+    check((nread = read(0, buffer, BUFSIZE)) == -1); // read from stdin
+    ptr = buffer; // pointer to buffer
+    ptr[nread-1] = '\0'; // add null terminator to buffer
 
-        check((nwritten = write(fd, ptr, nread)) <= 0); // write to socket and store number of bytes written
-        check(nread != nwritten); // check if wrote all bytes
-        ptr += nwritten; // move pointer nwritten bytes
+    check((nwritten = write(fd, ptr, nread)) <= 0); // write to socket and store number of bytes written
+    check(nread != nwritten); // check if wrote all bytes
+    ptr += nwritten; // move pointer nwritten bytes
 
-        // QUESTION: DOES THE SERVER REPLY IN 1 GO OR IN CHUNKS?
-        check((nread = read(fd, ptr, nwritten)) == -1); // read from socket TODO: CHANGE TO FORMATS
-        check(/*TODO: APANHAR ERRO DO SERVIDOR SE ELE FECHAR*/false);
-        check(nread != nwritten); // check if read all bytes
+    // QUESTION: DOES THE SERVER REPLY IN 1 GO OR IN CHUNKS?
+    check((nread = read(fd, ptr, nwritten)) == -1); // read from socket TODO: CHANGE TO FORMATS
+    check(/*TODO: APANHAR ERRO DO SERVIDOR SE ELE FECHAR*/false);
+    check(nread != nwritten); // check if read all bytes
 
-        write(1, "Server TCP: ", 12); 
-        write(1, ptr, nread); // print to stdout content from socket
-        write(1, "\n", 1); // print newline to stdout
-
-    }
+    write(1, "Server TCP: ", 12); 
+    write(1, ptr, nread); // print to stdout content from socket
+    write(1, "\n", 1); // print newline to stdout
 
     freeaddrinfo(res); // free address info
     close(fd);
@@ -129,9 +128,6 @@ int udp_client(char *asip, char *port) {
     int fd; //fd: file descriptor
     ssize_t nwritten, nread; //number of bytes written and read
     char *ptr, buffer[BUFSIZE]; //pointer to buffer and buffer to store data
-    
-    char message[BUFSIZE];
-    memset(message, "\0", BUFSIZE);
         
     check((fd = socket(AF_INET, SOCK_DGRAM, 0)) == -1); //UDP socket
 
@@ -143,27 +139,22 @@ int udp_client(char *asip, char *port) {
 
     check((connect(fd, res->ai_addr, res->ai_addrlen)) == -1); // connect to server
 
-    while(true) {
+    check((nread = read(0, buffer, BUFSIZE)) == -1); // read from stdin
+    ptr = buffer; // pointer to buffer
+    ptr[nread-1] = '\0'; // add null terminator to buffer
 
-        check((nread = read(0, buffer, BUFSIZE)) == -1); // read from stdin
-        ptr = buffer; // pointer to buffer
-        ptr[nread-1] = '\0'; // add null terminator to buffer
+    check((nwritten = write(fd, ptr, nread)) <= 0); // write to socket and store number of bytes written
+    check(nread != nwritten); // check if wrote all bytes
+    ptr += nwritten; // move pointer nwritten bytes
 
-        strcpy(message, udp_string_analysis(ptr));
+    check((nread = read(fd, ptr, nwritten)) == -1); // read from socket TODO: CHANGE TO FORMATS
+    check(/*TODO: APANHAR ERRO DO SERVIDOR SE ELE FECHAR*/false);
+    check(nread != nwritten); // check if read all bytes
 
-        check((nwritten = write(fd, ptr, nread)) <= 0); // write to socket and store number of bytes written
-        check(nread != nwritten); // check if wrote all bytes
-        ptr += nwritten; // move pointer nwritten bytes
+    write(1, "Server UDP: ", 12);
+    write(1, ptr, nread); // print to stdout content from socket
+    write(1, "\n", 1); // print newline to stdout
 
-        check((nread = read(fd, ptr, nwritten)) == -1); // read from socket TODO: CHANGE TO FORMATS
-        check(/*TODO: APANHAR ERRO DO SERVIDOR SE ELE FECHAR*/false);
-        check(nread != nwritten); // check if read all bytes
-
-        write(1, "Server UDP: ", 12); 
-        write(1, ptr, nread); // print to stdout content from socket
-        write(1, "\n", 1); // print newline to stdout
-
-    }
 
 }
 
@@ -171,17 +162,28 @@ int main(int argc, char *argv[]) {
 
     (void) argc; // unused
     (void) argv; // unused
+    char asip[32] = "localhost";
+    char port[6] = "58070";
+
+    ssize_t nread; // number of bytes read
+    char *ptr, buffer[BUFSIZE]; // pointer to buffer and buffer to store data
 
     struct sigaction stop; // CTRL_C signal handler
-
     memset(&stop, 0, sizeof(stop)); // initialize signal handler to 0s
     stop.sa_handler = safe_stop; // set handler to safe_stop function
     check(sigaction(SIGINT, &stop, NULL) == -1); // set signal handler to safe_stop for SIGINT
 
-    char asip[32] = "localhost";
-    char port[6] = "58070";
+    while(true) {
+        
+        check((nread = read(0, buffer, BUFSIZE)) == -1); // read from stdin
+        ptr = buffer; // pointer to buffer
+        ptr[nread-1] = '\0'; // add null terminator to buffer
 
-    udp_client(asip, port);
+        strcpy(buffer, string_analysis(ptr));
+
+        cout << buffer << endl;
+
+    }
     
     exit(0);
 
